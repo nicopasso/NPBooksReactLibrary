be8900f7c0824025819f9e2b8b41e14d6f7d90d3
nicopasso <nicopasso@gmail.com>
2015-05-04 16:31:05 +0200

    Search

---
+++ Components/BookDetails.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  Text,
+  View,
+  Component,
+  Image
+} = React;
+
+class BookDetails extends Component {
+  render() {
+
+    var book = this.props.book;
+    var imageURI = (typeof book.volumeInfo.imageLinks !== 'undefined') ? book.volumeInfo.imageLinks.thumbnail : '';
+    var description = (typeof book.volumeInfo.description !== 'undefined') ? book.volumeInfo.description : '';
+
+    return (
+
+      <View style={styles.container}>
+        <Image style={styles.image}
+          source={{uri: imageURI}} />
+          <Text style={styles.description}>{description}</Text>
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop: 75,
+    alignItems: 'center'
+  },
+  image: {
+    width: 107,
+    height: 165,
+    padding: 10
+  },
+  description: {
+    padding: 10,
+    fontSize: 15,
+    color: '#656565'
+  }
+});
+
+module.exports = BookDetails;
